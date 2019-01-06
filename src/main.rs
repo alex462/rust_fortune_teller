35ccf0df15cb84774d7654f84ddaa445940d9f9c
alex462 <alex.reinhart462@gmail.com>
2019-01-06 14:34:10 -0500

    taking user input and directing to appropriate method using deref trait instead of matching. only added possible yes input so far. successfully moves to second method again.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,25 @@ fn main() {
     println!("Do you want to play a game? y/n: ");
 
     let mut user_plays = String::new();
-    let opt_in = "y";
-    let opt_out = "n";
 
     io::stdin().read_line(&mut user_plays)
         .expect("Failed to read line");
 
-    if user_plays == opt_in {
-        play_game();
-    }
-    if user_plays == opt_out {
-        println!("Human forfeits. Computer wins by default.");
-    }
+    let opt_in = String::from("y");
+    let opt_out = String::from("n");
+
+    takes_str(&opt_in);
+
+    // if user_plays == opt_in {
+    //     play_game();
+    // }
+    // if user_plays == opt_out {
+    //     println!("Human forfeits. Computer wins by default.");
+    // }
 }
 
-fn play_game() {
+// fn play_game() {
+fn takes_str(opt_in: &str) {
     let fortunes = ["run.", "No snowflake in an avalanche ever feels responsible.",
         "Your pet is planning to eat you.",
         "Your resemblance to a muppet will prevent the world from taking you seriously.",
@@ -29,3 +33,4 @@ fn play_game() {
 
     println!("{}", fortunes[7]); //should output 8th fortune in array of fortunes
 }
+// }
