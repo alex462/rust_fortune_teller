e29c66f37dde4dbd2b458c008fb383b3160bb46d
alex462 <alex.reinhart462@gmail.com>
2019-01-07 02:21:46 -0500

    trying matching with slices. Matching unsuccessful.  Next will try to convert strings to string slices.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// use std::env::args;
+// use std::convert::AsRef;
 use std::io;
 
 fn main() {
@@ -9,25 +11,33 @@ fn main() {
         .read_line(&mut user_plays)
         .expect("Failed to read line");
 
-    if user_plays == String::from("y") {
-        takes_str_yes(&user_plays);
-    } else {
-        takes_str_no(&user_plays);
+    match &user_plays[..] {
+        "y" => println!("FUCK YEA"),
+        "n" => println!("HELL NO"),
+        _ => println!("askdfjwkejfkf"),
     }
+
+    // match &*str {
+    // if user_plays == String::from("y") {
+    //     takes_str_yes(&user_plays);
+    // } else {
+    //     takes_str_no(&user_plays);
+    // }
+    // }
 }
 
-fn takes_str_yes(_user_plays: &str) {
-    let fortunes = ["run.", "No snowflake in an avalanche ever feels responsible.",
-        "Your pet is planning to eat you.",
-        "Your resemblance to a muppet will prevent the world from taking you seriously.",
-        "If you eat something, and nobody sees you eat it, it has no calories.",
-        "If you think no one cares if you are alive, try missing a couple car payments.",
-        "A true patriot is the fellow who gets a parking ticket and rejoices that the system works. Are you a TRUE patriot?,",
-        "Your friends secretly agree that your head is the wrong size for your body."];
+// fn takes_str_yes(_user_plays: &str) {
+//     let fortunes = ["run.", "No snowflake in an avalanche ever feels responsible.",
+//         "Your pet is planning to eat you.",
+//         "Your resemblance to a muppet will prevent the world from taking you seriously.",
+//         "If you eat something, and nobody sees you eat it, it has no calories.",
+//         "If you think no one cares if you are alive, try missing a couple car payments.",
+//         "A true patriot is the fellow who gets a parking ticket and rejoices that the system works. Are you a TRUE patriot?,",
+//         "Your friends secretly agree that your head is the wrong size for your body."];
 
-    println!("{}", fortunes[7]); //should output 8th fortune in array of fortunes
-}
+//     println!("{}", fortunes[7]); //should output 8th fortune in array of fortunes
+// }
 
-fn takes_str_no(_user_plays: &str) {
-    println!("Human forfeits. Computer wins by default.");
-}
+// fn takes_str_no(_user_plays: &str) {
+//     println!("Human forfeits. Computer wins by default.");
+// }
