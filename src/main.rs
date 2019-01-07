5ed5346f0ce9fab077072e84f2b827fd6a0761be
alex462 <alex.reinhart462@gmail.com>
2019-01-06 23:11:16 -0500

    created separate methods for user game play decision, and trying to add if/else, but all input leads flow past game, on to final method, then out.

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,18 @@ fn main() {
 
     let mut user_plays = String::new();
 
-    io::stdin().read_line(&mut user_plays)
+    io::stdin()
+        .read_line(&mut user_plays)
         .expect("Failed to read line");
 
-    let opt_in = String::from("y");
-    let opt_out = String::from("n");
-
-    takes_str(&opt_in);
-
-    // if user_plays == opt_in {
-    //     play_game();
-    // }
-    // if user_plays == opt_out {
-    //     println!("Human forfeits. Computer wins by default.");
-    // }
+    if user_plays == String::from("y") {
+        takes_str_yes(&user_plays);
+    } else {
+        takes_str_no(&user_plays);
+    }
 }
 
-// fn play_game() {
-fn takes_str(opt_in: &str) {
+fn takes_str_yes(_user_plays: &str) {
     let fortunes = ["run.", "No snowflake in an avalanche ever feels responsible.",
         "Your pet is planning to eat you.",
         "Your resemblance to a muppet will prevent the world from taking you seriously.",
@@ -33,4 +27,7 @@ fn takes_str(opt_in: &str) {
 
     println!("{}", fortunes[7]); //should output 8th fortune in array of fortunes
 }
-// }
+
+fn takes_str_no(_user_plays: &str) {
+    println!("Human forfeits. Computer wins by default.");
+}
